12f24d3470a3d04ca4cdfc77f421c6e532dd4e75
Johncharly17 <diazmorato17@gmail.com>
2026-02-12 08:25:23 -0600

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -87,7 +87,7 @@ export const PROFESSIONALS: Professional[] = [
 ];
 
 export const TIME_SLOTS = [
-  '10:00', '11:00', '12:00', '13:00', '15:00', '16:00', '17:00', '18:00', '19:00'
+  '13:00', '15:00', '16:00', '17:00', '18:00', '19:00'
 ];
 
 export const COMPANY_INFO = {
