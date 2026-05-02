d5324ce7e16381e14141da266607ad4bb667342b
JohnCharlie17 <diazmorato@hotmail.com>
2026-05-02 09:28:09 -0600

    Conexion supabase

--- test_barbers.ts
+++ test_barbers.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+git import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://exfrgqafwkgjswenalys.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4ZnJncWFmd2tnanN3ZW5hbHlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MjU0NDIsImV4cCI6MjA4NjMwMTQ0Mn0.8ITBUgfsEcwc8jARsht5qlG898Sa_CqhDvjOzk9F9QI';
